af2167a9c4d4c47e3db90c236ddacd5b3269a95b
Mateus Paegle <mateus.martinegle@gmail.com>
2025-09-24 13:20:18 -0300

    test: improve email validation logic in SignUpController

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -136,4 +136,21 @@ describe("Signup Controller", () => {
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
   });
+
+  test("should return 400 if password confirmation fails", async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "invalid_password",
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new InvalidParamError("passwordConfirmation"),
+    );
+  });
 });
