54be345da13e7493978d470de543a780af5cb398
Mateus Paegle <mateus.martinegle@gmail.com>
2025-09-24 13:40:28 -0300

    test: clean up SignUpController code structure and improve readability

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -30,7 +30,7 @@ const makeAddAccount = (): AddAccount => {
       const fakeAccount = {
         id: "valid_id",
         name: "valid_name",
-        email: "valid_email@mail.com",
+        email: "valid@email.com",
         password: "valid_password",
       };
 
@@ -219,5 +219,26 @@ describe("Signup Controller", () => {
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
+  });
+
+  test("should return 200 if valid data is provided", async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: "valid_name",
+        email: "valid@email.com",
+        password: "valid_password",
+        passwordConfirmation: "valid_password",
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: "valid_id",
+      name: "valid_name",
+      email: "valid@email.com",
+      password: "valid_password",
+    });
   });
 });
