eb5485d0ba7305685cee242d7d8a947776466baa
Mateus Paegle <mateus.martinegle@gmail.com>
2025-09-24 13:10:43 -0300

    test: create index file to export error classes

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,5 @@
 import { describe, expect, test, vi } from "vitest";
-import { InvalidParamError } from "../errors/invalid-param.error";
-import { MissingParamError } from "../errors/missing-param.error";
-import { ServerError } from "../errors/server.error";
+import { InvalidParamError, MissingParamError, ServerError } from "../errors";
 import type { EmailValidator } from "../protocols/email-validator";
 import { SignUpController } from "./signup.controlle";
 
