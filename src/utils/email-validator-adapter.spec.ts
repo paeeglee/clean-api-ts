1006be412e2c669c4d1bb5f78ee1a36a1c331ae7
Mateus Paegle <mateus.martinegle@gmail.com>
2025-09-25 08:42:28 -0300

    test: add email validation using validator library

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,10 +1,28 @@
-import { describe, expect, test } from "vitest";
+import validator from "validator";
+import { describe, expect, test, vi } from "vitest";
+
 import { EmailValidatorAdapter } from "./email-validator";
+
+vi.mock("validator", () => ({
+  default: {
+    isEmail(): boolean {
+      return true;
+    },
+  },
+}));
 
 describe("EmailValidator Adapter", () => {
   test("should retun flase if validator returns false", () => {
+    vi.spyOn(validator, "isEmail").mockReturnValueOnce(false);
+
     const sut = new EmailValidatorAdapter();
     const isValid = sut.isValid("invalid_email@mail.com");
     expect(isValid).toBe(false);
+  });
+
+  test("should retun flase if validator returns true", () => {
+    const sut = new EmailValidatorAdapter();
+    const isValid = sut.isValid("valid_email@mail.com");
+    expect(isValid).toBe(true);
   });
 });
