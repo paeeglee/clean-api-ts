6f78d6fbcc002c6522abec26ff8321f6c21f109e
Mateus Paegle <mateus.martinegle@gmail.com>
2025-09-25 08:48:33 -0300

    test: implement EmailValidatorAdapter for email validation

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,7 +1,7 @@
 import validator from "validator";
 import { describe, expect, test, vi } from "vitest";
 
-import { EmailValidatorAdapter } from "./email-validator";
+import { EmailValidatorAdapter } from "./email-validator-adapter";
 
 vi.mock("validator", () => ({
   default: {
@@ -20,9 +20,17 @@ describe("EmailValidator Adapter", () => {
     expect(isValid).toBe(false);
   });
 
-  test("should retun flase if validator returns true", () => {
+  test("should retun false if validator returns true", () => {
     const sut = new EmailValidatorAdapter();
     const isValid = sut.isValid("valid_email@mail.com");
     expect(isValid).toBe(true);
+  });
+
+  test("should call validator with correct email", () => {
+    const isEmailSpy = vi.spyOn(validator, "isEmail");
+    const sut = new EmailValidatorAdapter();
+    sut.isValid("valid_email@mail.com");
+
+    expect(isEmailSpy).toHaveBeenCalledWith("valid_email@mail.com");
   });
 });
