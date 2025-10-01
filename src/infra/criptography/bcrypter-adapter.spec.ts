148a3d3bfba99c3a128d56b193ef44ee170f03a4
Mateus Paegle <mateus.martinegle@gmail.com>
2025-10-01 07:34:13 -0300

    test: correct return value in BcryptAdapter encrypt method

--- src/infra/criptography/bcrypter-adapter.spec.ts
+++ src/infra/criptography/bcrypter-adapter.spec.ts
@@ -2,6 +2,14 @@ import bcrypt from "bcrypt";
 import { describe, expect, it, vi } from "vitest";
 import { BcryptAdapter } from "./bcrypter-adapter";
 
+vi.mock("bcrypt", () => ({
+  default: {
+    async hash(): Promise<string> {
+      return new Promise((resolve) => resolve("hashed_value"));
+    },
+  },
+}));
+
 describe("Bcrypter Adapter", () => {
   it("should call bcrypt with correct value", async () => {
     const sut = new BcryptAdapter(12);
@@ -9,5 +17,11 @@ describe("Bcrypter Adapter", () => {
 
     await sut.encrypt("any_value");
     expect(hashSpy).toHaveBeenCalledWith("any_value", 12);
+  });
+
+  it("should return a hash on success", async () => {
+    const sut = new BcryptAdapter(12);
+    const hash = await sut.encrypt("any_value");
+    expect(hash).toBe("hashed_value");
   });
 });
