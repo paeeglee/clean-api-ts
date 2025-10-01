726b83c15cb1b497c15bc20763997e518946f685
Mateus Paegle <mateus.martinegle@gmail.com>
2025-10-01 07:29:36 -0300

    feat: add BcryptAdapter for password encryption and update dependencies

---
+++ src/infra/criptography/bcrypter-adapter.ts
@@ -0,0 +1,11 @@
+import bcrypt from "bcrypt";
+import type { Encrypter } from "../../data/protocols/encrypter.protocol";
+
+export class BcryptAdapter implements Encrypter {
+  constructor(private readonly salt: number = 12) {}
+
+  async encrypt(_value: string): Promise<string> {
+    await bcrypt.hash(_value, this.salt);
+    return "hashed_value";
+  }
+}
